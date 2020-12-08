b2aaf7c00bc079a32255c71d6a352c82dc6d030f
Danny Vosecky <dvosecky@protonmail.com>
2020-12-07 17:07:49 -0800

    basic drag and drop working, but lag bug after repeated drags

--- client/src/GamePage.js
+++ client/src/GamePage.js
@@ -5,9 +5,11 @@ import io from 'socket.io-client'
 function GamePage() {
 	const { id } = useParams()
 	const [joinFailed, setJoinFailed] = useState(false)
-	const [side, setSide] = useState('white')
+	const [side, setSide] = useState('black')
 	// const [squares, setSquares] = useState(null)
-	const pieces = [{ square: 'a2', type: 'pawn', player: 'white' }]
+	const [pieces, setPieces] = useState([{ square: 'a2', type: 'pawn', player: 'white' }])
+	const [absPos, setAbsPos] = useState({ position: 'absolute', top: 0, left: 0 })
+	const [draggedPiece, setDraggedPiece] = useState(null)
 
 	const squares = []
 
@@ -15,26 +17,21 @@ function GamePage() {
 	for (let i = 0; i < 8; i++) {
 		squares.push([])
 		for (let j = 0; j < 8; j++) {
-			squares[i].push(null)
+			const name = (side === 'white') ? 'abcdefgh'[j] + (8 - i) : 'hgfedcba'[j] + (i + 1)
+			squares[i].push({ piece: null, name })
 		}
 	}
 
 	// add pieces to squares
 	pieces.forEach((piece) => {
-		let [letter, number] = piece.square.split('')
-		number = parseInt(number)
-		let col = letter.charCodeAt() - 'a'.charCodeAt()
-		let row
-		if (side === 'white') {
-			row = 8 - number
-			col = letter.charCodeAt() - 'a'.charCodeAt()
-		} else if (side === 'black') {
-			row = number - 1
-			col = 7 - (letter.charCodeAt() - 'a'.charCodeAt())
-		}
-		squares[row][col] = { type: piece.type, player: piece.player }
+		squares.forEach((row) => {
+			row.forEach((square) => {
+				if (piece.square === square.name) {
+					square.piece = piece
+				}
+			})
+		})
 	})
-	
 
 
 	// useEffect(() => {
@@ -61,6 +58,39 @@ function GamePage() {
 
 	// }, [id])
 
+	const updateAbsPos = (e) => {
+		setAbsPos({
+			position: 'absolute',
+			pointerEvents: 'none',
+			left: e.pageX - window.innerHeight / 16 + 'px',
+			top: e.pageY - window.innerHeight / 16 + 'px'
+		})
+	}
+
+	const dragPiece = (e, square) => {
+		setDraggedPiece(square.piece)
+		updateAbsPos(e)
+		document.addEventListener('mousemove', updateAbsPos)
+	}
+
+	const dropPiece = (square => {
+		if (draggedPiece) {
+			console.log('up')
+			setPieces((pieces) => {
+				const newPieces = pieces.slice()
+				const index = newPieces.indexOf(draggedPiece)
+				console.log('index is ' , index)
+				console.log('square.name is ', square.name)
+				newPieces[index] = Object.assign(draggedPiece, { square: square.name })
+				return newPieces
+			})
+			setDraggedPiece(null)
+			document.removeEventListener('mousemove', updateAbsPos)
+		} else {
+			console.log(square.name)
+		}
+	})
+
 	return (
 		<div>
 			<h1>This is game {id}</h1>
@@ -69,16 +99,21 @@ function GamePage() {
 			{squares.map((row, rowNum) => {
 				return (
 					<div className="row" key={(rowNum + 1)}>{
-						row.map((square, colNum) => {
+						row.map((square) => {
 							return (
-								<div className="square" key={'abcdefgh'[colNum] + (rowNum + 1)}>
-									{square && <div className={square.player + '-' + square.type} />}
+								<div className="square" key={square.name} onMouseUp={() => dropPiece(square)}>
+									{square.piece && draggedPiece !== square.piece &&
+									<div className={square.piece.player + '-' + square.piece.type}
+										onMouseDown={(e) => dragPiece(e, square)}
+									/>}
 								</div>
 							)
 						})
 					}</div>
 				)
 			})}
+			{draggedPiece &&
+			<div className={draggedPiece.player + '-' + draggedPiece.type} style={absPos}/>}
 		</div>
 	)
 }
