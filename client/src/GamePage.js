2dd980a3c6087d6877ef57221e1dcb8eeba3869c
Danny Vosecky <dvosecky@protonmail.com>
2020-12-09 17:12:12 -0800

    touch and mouse events working, memoize squares

--- client/src/GamePage.js
+++ client/src/GamePage.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { Redirect, useParams } from 'react-router-dom'
 import io from 'socket.io-client'
 
@@ -6,123 +6,148 @@ function GamePage() {
 	const { id } = useParams()
 	const [joinFailed, setJoinFailed] = useState(false)
 	const [side, setSide] = useState()
-	const [pieces, setPieces] = useState([{ type: 'pawn', player: 'white', square: 'a1' }])
+	const [pieces, setPieces] = useState([])
 	const [absPos, setAbsPos] = useState({ position: 'absolute', top: 0, left: 0 })
 	const [draggedPiece, setDraggedPiece] = useState(null)
 
-	// connect to game
-	// useEffect(() => {
-	// 	const socket = io()
-	// 	socket.emit('join', id)
+	const squares = useMemo(() => {
+		const squares = []
+		for (let i = 0; i < 64; i++) {
+			const square = {}
+			// set square.id using algebraic notation
+			const row = Math.floor(i / 8)
+			const col = i % 8
+			square.id = (side === 'white') ? 'abcdefgh'[col] + (8 - row) : 'hgfedcba'[col] + (row + 1)
 
-	// 	socket.on('join failed', (reason) => {
-	// 		console.log('join failed: ' + reason)
-	// 		setJoinFailed(true) // will cause redirect to home
-	// 	})
-
-	// 	socket.on('game data', (game) => {
-	// 		if (game.white === socket.id) {
-	// 			setSide('white')
-	// 		} else if (game.black === socket.id) {
-	// 			setSide('black')
-	// 		}
-	// 		setPieces(game.pieces)
-	// 	})
-
-	// 	return function cleanup() {
-	// 		socket.disconnect()
-	// 	}
-
-	// }, [id])
-
-
-
-	const startDrag = (e, square) => {
-		setDraggedPiece(square.piece)
-		trackMouse(e)
-	}
-
-	const endDrag = (e) => {
-		if (draggedPiece) {
-			// find the new square
-			const square = squares.find((square) => {
-				return square.id === document.elementFromPoint(e.changedTouches[0].clientX, e.changedTouches[0].clientY).id
+			// if there is a piece on this square, add a reference to the piece object
+			square.piece = null
+			pieces.forEach((piece) => {
+				if (piece.square === square.id) {
+					square.piece = piece
+				}
 			})
-
-			// if square was found, place piece there
-			if (square) {
-				setPieces((pieces) => {
-					const newPieces = pieces.slice()
-					const index = newPieces.indexOf(draggedPiece)
-					newPieces[index] = Object.assign(draggedPiece, { square: square.id })
-					return newPieces
-				})
-			}
-
-			setDraggedPiece(null)
+			squares.push(square)
 		}
-	}
+		return squares
+	}, [pieces, side])
 
-	// set squares
-	const squares = []
-	for (let i = 0; i < 64; i++) {
-		const square = {}
-		const row = Math.floor(i / 8)
-		const col = i % 8 
-		square.id = (side === 'white') ? 'abcdefgh'[col] + (8 - row) : 'hgfedcba'[col] + (row + 1)
-		square.piece = null
 
-		// if there is a piece on this square add it
-		pieces.forEach((piece) => {
-			if (piece.square === square.id) {
-				square.piece = piece
-			}
-		})
-		squares.push(square)
-	}
-
-	const page = useRef()
-
-	const trackMouse = (e) => {
-		const offset = Math.min(window.innerWidth, window.innerHeight) / 16
-		setAbsPos({
-			position: 'absolute',
-			pointerEvents: 'none',
-			left: e.changedTouches[0].pageX - offset + 'px',
-			top: e.changedTouches[0].pageY - offset + 'px'
-		})
-	}
-
+	// connect to game
 	useEffect(() => {
-		const touchMove = (e) => {
+		const socket = io()
+		socket.emit('join', id)
+
+		socket.on('join failed', (reason) => {
+			console.log('join failed: ' + reason)
+			setJoinFailed(true) // will cause redirect to home
+		})
+
+		socket.on('game data', (game) => {
+			if (game.white === socket.id) {
+				setSide('white')
+			} else if (game.black === socket.id) {
+				setSide('black')
+			}
+			setPieces(game.pieces)
+		})
+
+		return function cleanup() {
+			socket.disconnect()
+		}
+
+	}, [id])
+
+	// using native DOM events to allow preventDefault() with TouchEvents (non-passive)
+	// these events are for drag-and-dropping pieces
+	const page = useRef()
+	useEffect(() => {
+
+		const trackPointer = (e) => {
+			// update the absolute position styling applied to the dragged piece
+			const offset = Math.min(window.innerWidth, window.innerHeight) / 16
+			const x = e.changedTouches ? e.changedTouches[0].pageX : e.pageX
+			const y = e.changedTouches ? e.changedTouches[0].pageY : e.pageY
+			setAbsPos({
+				position: 'absolute',
+				left: x - offset + 'px',
+				top: y - offset + 'px'
+			})
+		}
+		
+		const startDrag = (e) => {
+			// find the square the pointer is over
+			const pointerX = e.changedTouches ? e.changedTouches[0].clientX : e.clientX
+			const pointerY = e.changedTouches ? e.changedTouches[0].clientY : e.clientY
+			const elementId = document.elementFromPoint(pointerX, pointerY).id
+			const square = squares.find((square) => square.id === elementId)
+			if (square && square.piece) {
+				// start dragging the piece on the square
+				e.preventDefault()
+				setDraggedPiece(square.piece)
+				trackPointer(e)
+			}
+		}
+		
+		const endDrag = (e) => {
+			if (draggedPiece) {
+				// find the square the pointer is over
+				e.preventDefault()
+				const pointerX = e.changedTouches ? e.changedTouches[0].clientX : e.clientX
+				const pointerY = e.changedTouches ? e.changedTouches[0].clientY : e.clientY
+				const elementId = document.elementFromPoint(pointerX, pointerY).id
+				const square = squares.find((square) => {
+					return square.id === elementId
+				})
+				if (square && !square.piece) {
+					// set the dropped piece's new square
+					setPieces((pieces) => {
+						const newPieces = pieces.slice()
+						const index = newPieces.indexOf(draggedPiece)
+						newPieces[index] = Object.assign(draggedPiece, { square: square.id })
+						return newPieces
+					})
+				}
+				setDraggedPiece(null)
+			}
+		}
+		
+		const moveDrag = (e) => {
 			if (draggedPiece) {
 				e.preventDefault()
-				trackMouse(e)
+				trackPointer(e)
 			}
 		}
 
 		const pageElement = page.current
-		pageElement.addEventListener('touchmove', touchMove, { passive: false })
+		pageElement.addEventListener('touchmove', moveDrag, { passive: false })
+		pageElement.addEventListener('mousemove', moveDrag)
+		pageElement.addEventListener('touchstart', startDrag, { passive: false })
+		pageElement.addEventListener('mousedown', startDrag)
+		pageElement.addEventListener('touchend', endDrag, { passive: false })
+		pageElement.addEventListener('mouseup', endDrag)
+
 		return (() => {
-			pageElement.removeEventListener('touchmove', touchMove, { passive: false })
+			pageElement.removeEventListener('touchmove', moveDrag, { passive: false })
+			pageElement.removeEventListener('mousemove', moveDrag)
+			pageElement.removeEventListener('touchstart', startDrag, { passive: false })
+			pageElement.removeEventListener('mousedown', startDrag)
+			pageElement.removeEventListener('touchend', endDrag, { passive: false })
+			pageElement.removeEventListener('mouseup', endDrag)
 		})
-	}, [draggedPiece])
+	}, [squares, draggedPiece])
 
 	return (
-		<div
-			ref={page}
-			onTouchEnd={endDrag}
-		>
+		<div ref={page}>
 			<h1>This is game {id}</h1>
 			<h2>You are playing as {side}</h2>
 			{joinFailed && <Redirect to="/"/>}
-			<div id="board" draggable="false">
+
+			<div id="board">
 				{squares.map((square) => {
 					return (
-						<div id={square.id} className="square" key={square.id} draggable="false">
+						<div id={square.id} className="square" key={square.id}>
 							{square.piece &&
 							<div className={'piece ' + square.piece.player + '-' + square.piece.type}
-								onTouchStart={(e) => startDrag(e, square)}
 								style={draggedPiece === square.piece ? absPos : {}}
 							/>}
 						</div>
